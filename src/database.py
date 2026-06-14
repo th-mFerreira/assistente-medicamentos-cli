75502bee53e1d40ce74fc0eca75abe47a3877901
Miguel Ferreira <ferreira@sempreceub.com>
2026-06-14 16:58:34 -0300

    eliminando duplicidade de codigo e corrigindo chamadas de ambiente

--- src/database.py
+++ src/database.py
@@ -2,12 +2,13 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
+# Carrega as variáveis de ambiente do ficheiro .env
 load_dotenv()
 
 def get_supabase_client() -> Client:
     """Instancia e retorna o cliente de comunicação com o Supabase."""
-    url = os.environ.get("SUPABASE_URL")
-    key = os.environ.get("SUPABASE_KEY")
+    url = os.environ.get("https://qlrwqzyztxwhuqkzwpzk.supabase.co")
+    key = os.environ.get("sb_publishable_LNjB1Cvb6L2Yz5vlX2crAg_cscvCLJw")
     
     if not url or not key:
         raise ValueError("Credenciais do Supabase ausentes no ambiente de execução.")
