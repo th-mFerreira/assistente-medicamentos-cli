f866c8c1b2aa68a6471de8c678527549f5519f65
Miguel Ferreira <ferreira@sempreceub.com>
2026-06-14 15:29:49 -0300

    feat: adicionando cliente do supabase e arquivo de configuracao

---
+++ src/database.py
@@ -0,0 +1,14 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_supabase_client() -> Client:
+    url = os.environ.get("https://qlrwqzyztxwhuqkzwpzk.supabase.co")
+    key = os.environ.get("sb_publishable_LNjB1Cvb6L2Yz5vlX2crAg_cscvCLJw")
+    
+    if not url or not key:
+        raise ValueError("Credenciais do Supabase ausentes no ambiente de execução.")
+        
+    return create_client(url, key)
